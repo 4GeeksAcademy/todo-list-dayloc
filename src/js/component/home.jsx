9e54ed56ee452120eced25d182e87eb0cd790fd9
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-04-23 20:24:41 +0000

    actualizando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,38 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const validateTask= (task) =>{
+    if (!task|| !task.trim()){
+      alert('El Campo de tarea es obligatorio')
+    }
+    setTask([...tasks, task]);setTask('');
+  }
+  const delateTask = (task)=>{
+    const newTasks = task.filter((item) => item !== task);
+    setTask([...newTasks]) 
+    console.log(Tasks)
+  }
+  return (
+    <div className="container  ">
+      <h1>My Todos</h1>
+      <div className="todolist">
+        <input type="text" placeholder="Agregar Tarea"
+        onChange={(evt) => setTask(evt.target.value)}
+        onKeyDown={(evt) => evt.key ==='Enter' && validateTask(task)}
+        value={task}
+        />
+        <div>
+          {
+          tasks.map((item, index) => <p key={index}> {item}<span onClick={()=> delateTask(item)}>X</span></p>)
+          }
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default Home;
