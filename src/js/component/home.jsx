58ff0e40e402b8e59e212241b3cce91bf6695cc0
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-04-26 19:22:43 +0000

    Al fin

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,35 +2,43 @@ import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-  const [task, setTask] = useState("");
+  const [task,setTask] = useState('');
   const [tasks, setTasks] = useState([]);
-  const validateTask= (task) =>{
-    if (!task|| !task.trim()){
-      alert('El Campo de tarea es obligatorio')
-    }
-    setTask([...tasks, task]);setTask('');
-  }
-  const delateTask = (task)=>{
-    const newTasks = task.filter((item) => item !== task);
-    setTask([...newTasks]) 
-    console.log(Tasks)
-  }
-  return (
-    <div className="container  ">
-      <h1>My Todos</h1>
-      <div className="todolist">
-        <input type="text" placeholder="Agregar Tarea"
-        onChange={(evt) => setTask(evt.target.value)}
-        onKeyDown={(evt) => evt.key ==='Enter' && validateTask(task)}
-        value={task}
-        />
-        <div>
-          {
-          tasks.map((item, index) => <p key={index}> {item}<span onClick={()=> delateTask(item)}>X</span></p>)
-          }
-        </div>
 
+  
+ const validateTask = (task) => {
+ if (!task || !task.trim()) {
+     alert("El Campo de tarea es obligatorio.");
+  }
+    setTasks([...tasks, task]);
+   
+    setTask('');
+ }
+  
+  const delateTask = (task) => {
+    const newTasks = tasks.filter((item) => item !== task);
+    setTasks([...newTasks]);
+    };
+  return (
+    <div className="container text-center">
+      <h1>Lista de Tareas</h1>
+      <div className="tolist">
+        <input placeholder="Agregar Tarea" 
+        onChange={(evt) => setTask (evt.target.value)}
+        onKeyDown={(evt) => evt.key ==='Enter' && validateTask (task)}  
+        value={task}
+        type="text" />
+
+      </div >
+
+      <div className=" mt-3">
+      <ul >
+          {
+            tasks.map((item,index) => <li className="list"  key={index}>{index+1} .    {item} <button onClick={()=> delateTask(item)}>X</button></li>)
+          }     
+      </ul>
       </div>
+      <p className="footer">Tienes <span className="pendientes">{tasks.length}</span> tareas pendientes.    </p>
     </div>
   );
 };
